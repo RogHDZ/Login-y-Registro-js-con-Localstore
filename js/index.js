788717ea8039b8ea91dd86e5639c34f2eb50316e
rogeliohdz <rogeliohernandezserafin>
2022-10-20 23:37:28 -0500

    validando login

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const usuario = document.getElementById('user');
+const password = document.getElementById('pass');
+const formulario = document.getElementById('formLogin');
+
+formulario.addEventListener('submit', login);
+
+function login(e){
+    e.preventDefault();
+
+    let usuarioVal = usuario.value;
+    let passwordVal = password.value;
+
+    if(usuarioVal == '' || passwordVal == ''){
+        
+        crearMensaje('Verifica tus campos', 'danger');
+        return;
+    }
+
+    if(localStorage.getItem('usuario')){
+        let objeto = JSON.parse(localStorage.getItem('usuario'));
+
+        if(usuarioVal == objeto.user && passwordVal == objeto.pass){
+            crearMensaje('Login correcto', 'success');
+        } else {
+            crearMensaje('Usuario incorrecto', 'danger');
+        }
+    } else {
+        crearMensaje('No hay registros', 'danger');
+    }
+}
--- js/registro.js
+++ js/registro.js
@@ -37,13 +37,3 @@ function registro(e){
     crearMensaje('Usuario Registrado!', 'success');
 }
 
-function crearMensaje(texto, tipo){
-    const nuevoElemento = document.createElement('div');
-    nuevoElemento.innerText = texto;
-    nuevoElemento.classList.add('alert', 'alert-'+tipo);
-    const divMensaje = document.getElementById('mensaje');
-    divMensaje.appendChild(nuevoElemento);
-    setTimeout(function(){
-        nuevoElemento.remove();
-    }, 2000);
-}
